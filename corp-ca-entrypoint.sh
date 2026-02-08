dbb2a211d5e1bda4d48746a914b34749c07d28da
dfirr <swat@dfirr.com>
2026-02-08 11:48:48 +0000

    Remove CA entrypoint

--- corp-ca-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-CERT_DIR="/usr/local/share/ca-certificates/corp"
-if [ -d "${CERT_DIR}" ] && find "${CERT_DIR}" -type f -name '*.crt' -print -quit | grep -q .; then
-  update-ca-certificates
-fi
-
-exec "$@"
